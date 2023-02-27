802d52cf1e6531fe67b6a9a7f1717fc3a5b5cbdc
xiaoweishen <1836447715@qq.com>
2023-02-27 16:34:58 +0800

    finished rotation getList Fuction

--- internal/logic/rotation/rotation.go
+++ internal/logic/rotation/rotation.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"gongzhaoweishop/internal/dao"
 	"gongzhaoweishop/internal/model"
+	"gongzhaoweishop/internal/model/entity"
 	"gongzhaoweishop/internal/service"
 )
 
@@ -39,7 +40,9 @@ func (s *sRotation) Delete(ctx context.Context, id uint) error {
 		// 删除内容
 		_, err := dao.RotationInfo.Ctx(ctx).Where(g.Map{
 			dao.RotationInfo.Columns().Id: id,
-		}).Unscoped().Delete()
+			//Unscoped 实际删除
+			//}).Unscoped().Delete()
+		}).Delete()
 		return err
 	})
 }
@@ -59,4 +62,76 @@ func (s *sRotation) Update(ctx context.Context, in model.RotationUpdateInput) er
 			Update()
 		return err
 	})
+}
+
+// GetList 查询内容列表
+func (s *sRotation) GetList(ctx context.Context, in model.RotationGetListInput) (out *model.RotationGetListOutput, err error) {
+	var (
+		m = dao.RotationInfo.Ctx(ctx)
+	)
+	out = &model.RotationGetListOutput{
+		Page: in.Page,
+		Size: in.Size,
+	}
+	// 默认查询topic
+	//if in.Type != "" {
+	//	m = m.Where(dao.Rotation.Columns().Type, in.Type)
+	//} else {
+	//	m = m.Where(dao.Rotation.Columns().Type, consts.RotationTypeTopic)
+	//}
+	// 栏目检索
+	//if in.CategoryId > 0 {
+	//	//返回数据的示例
+	//	idArray, err := service.Category().GetSubIdList(ctx, in.CategoryId)
+	//	if err != nil {
+	//		return out, err
+	//	}
+	//	//where in 查询
+	//	m = m.Where(dao.Rotation.Columns().CategoryId, idArray)
+	//}
+	// 管理员可以查看所有文章
+	//if in.UserId > 0 && !service.User().IsAdmin(ctx, in.UserId) {
+	//	m = m.Where(dao.Rotation.Columns().UserId, in.UserId)
+	//}
+	// 分配查询
+	listModel := m.Page(in.Page, in.Size)
+	// 排序方式
+	listModel = listModel.OrderDesc(dao.RotationInfo.Columns().Sort)
+
+	var list []*entity.RotationInfo
+	if err := listModel.Scan(&list); err != nil {
+		return out, err
+	}
+	// 没有数据
+	if len(list) == 0 {
+		return out, nil
+	}
+	out.Total, err = m.Count()
+	if err != nil {
+		return out, err
+	}
+	// Rotation
+	//指定item的键名用：ScanList
+	//if err := listModel.ScanList(&out.List, "Rotation"); err != nil {
+	//不指定item的键名用：Scan
+	if err := listModel.Scan(&out.List); err != nil {
+		return out, err
+	}
+	//// Category
+	//err = dao.Category.Ctx(ctx).
+	//	Fields(model.RotationListCategoryItem{}).
+	//	Where(dao.Category.Columns().Id, gutil.ListItemValuesUnique(out.List, "Rotation", "CategoryId")).
+	//	ScanList(&out.List, "Category", "Rotation", "id:CategoryId")
+	//if err != nil {
+	//	return out, err
+	//}
+	//// User
+	//err = dao.User.Ctx(ctx).
+	//	Fields(model.RotationListUserItem{}).
+	//	Where(dao.User.Columns().Id, gutil.ListItemValuesUnique(out.List, "Rotation", "UserId")).
+	//	ScanList(&out.List, "User", "Rotation", "id:UserId")
+	//if err != nil {
+	//	return out, err
+	//}
+	return
 }
